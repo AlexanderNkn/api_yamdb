0c63283bbf7d218df9cf06205ac4e88e27c2fa94
Alexander <spresi@mail.ru>
2020-05-13 10:14:32 +0300

    fixed content permissions

--- contents/permissions.py
+++ contents/permissions.py
@@ -5,7 +5,7 @@ class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return request.user.role in ["moderator", "admin"] or request.user.is_superuser
+        return request.user.is_superuser
 
     def has_object_permission(self, request, view, obj):
         return self.has_permission(request, view)
